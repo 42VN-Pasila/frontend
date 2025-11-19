5ad7b582d0e73d6783500d75ba040878b0500135
Triet Pham <pmtriet1211@gmail.com>
2025-11-19 21:34:25 +0200

    change color of animation

--- src/pages/HomePage/PongAnimation.tsx
+++ src/pages/HomePage/PongAnimation.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const PongAnimation: React.FC = () => {
   return (
-    <div className="relative mx-auto w-[600px] h-[300px] rounded-[5px] border-b-[5px] border-t-[5px] border-[#FF5F24] border-opacity-50">
-      <div className="absolute top-0 left-0 w-[7px] h-[90px] bg-[#ccac54] rounded-full animate-left-wall" />
+    <div className="relative mx-auto w-[600px] h-[300px] rounded-[5px] border-[#2f4858] border-opacity-50">
+      <div className="absolute top-0 left-0 w-[7px] h-[90px] bg-[#ff315b] rounded-full animate-left-wall" />
 
-      <div className="absolute top-0 right-0 w-[7px] h-[90px] bg-[#61AE24] rounded-full  animate-right-wall" />
+      <div className="absolute top-0 right-0 w-[7px] h-[90px] bg-[#6855d0] rounded-full  animate-right-wall" />
 
       <div
         className="
@@ -16,11 +16,11 @@ const PongAnimation: React.FC = () => {
           h-[220px]
           w-[3px]
           bg-[#F0E8E5]
-          opacity-50
+          opacity-25
           rounded-full
         "
       />
-      <div className="absolute top-[30px] left-0 w-[20px] h-[20px] bg-[#F0E8E5] opacity-80 rounded-full animate-bounce-ball" />
+      <div className="absolute top-[30px] left-0 w-[20px] h-[20px] bg-[#ff5f24] opacity-80 rounded-full animate-bounce-ball" />
     </div>
   );
 };
