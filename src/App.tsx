4891933db6159751639eb3e13e1794c65bcb0dc1
htran312 <huongcake312@gmail.com>
2025-12-27 21:04:38 +0200

    feat: add About Us page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/Auth/RegisterPage";
 import LoginPage from "./pages/Auth/LoginPage";
 // import { ProtectedRoute } from "./shared/components/ProtectedRoute";
+import AboutUsPage from "./pages/AboutUsPage/AboutUsPage"
 // import DashBoard from "./pages/DashBoard/DashBoard";
 
 const App: React.FC = () => {
@@ -22,6 +23,8 @@ const App: React.FC = () => {
               </ProtectedRoute>
             }
           /> */}
+          {/* <Route path="/dashboard" element={<DashBoard/>}/> */}
+          <Route path="/about-us" element={<AboutUsPage />} />
         </Routes>
       </main>
     </div>
---
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/shared/components/Footer";
+
+const AboutUsPage = () => {
+    return (
+        <main className="flex flex-col min-h-[100dvh]">
+            <Footer />
+        </main>
+    )
+}
+
+export default AboutUsPage;
