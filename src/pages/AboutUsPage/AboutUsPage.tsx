daaa1ecd28208374b5de792ad28324a38c2177b6
htran312 <huongcake312@gmail.com>
2025-12-27 22:28:35 +0200

    About us is currently "borrowing" the navigation bar from Home page. TODO: - NavBar as a shared component. - Typography as a shared component.

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,8 +1,10 @@
 import Footer from "@/shared/components/Footer";
+import HomePageNavBar from "../HomePage/HomePageNavBar";
 
 const AboutUsPage = () => {
     return (
         <main className="flex flex-col min-h-[100dvh]">
+            <HomePageNavBar />
             <Footer />
         </main>
     )
