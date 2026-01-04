94a79e3ed97ccff3e0a7c600843c12ece35f0faa
Cao Tan Nguyen <caonguye@c2r3p12.hive.fi>
2026-01-04 16:25:13 +0200

    update redirection logo

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -1,10 +1,14 @@
 import logoSrc from "../../assets/jokercard.png";
 import { Link } from "react-router-dom";
+import { isExpired } from "../utils/sessionVerify";
 
 const Logo = () => {
+  const accessToken = localStorage.getItem('accessToken');
+
+  const redirectPath = ((accessToken && !isExpired(accessToken))? '/dashboard' : '/');
   return (
     <div>
-      <Link to="/" className="inline-flex items-center gap-2">
+      <Link to={redirectPath} className="inline-flex items-center gap-2">
         <img src={logoSrc} alt="Logo" className="h-23 xs:h-24 py-2" />
 
         <span
