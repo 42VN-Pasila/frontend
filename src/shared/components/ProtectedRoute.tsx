9b3eca84b26ddd01102be40f8ea76fca0158bdee
Siuol <nguyencaotan03072000@gmail.com>
2026-01-08 10:02:53 +0200

    Update completed Protected Route

--- src/shared/components/ProtectedRoute.tsx
+++ src/shared/components/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import { sessionVerify } from "../utils/sessionVerify";
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const [isAuthenticated, setAuthenticated] = useState<boolean>(false);
+  const [isAuthenticated, setAuthenticated] = useState<boolean | null >(null);
 
   useEffect(() => {
     const checkAuth = async () => {
@@ -12,4 +12,13 @@ export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     };
     checkAuth();
   }, []);
+
+  if (isAuthenticated === null){
+    return <div>Loading...</div>
+  }
+
+  if (isAuthenticated){
+    return <>{children}</>;
+  }
+  return <Navigate to='/login' replace/>;
 };
