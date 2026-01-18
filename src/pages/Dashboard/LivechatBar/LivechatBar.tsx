c94d201362acf9e7fc94ff0e46fb9ea8179227f7
Siuol <nguyencaotan03072000@gmail.com>
2026-01-18 18:42:01 +0200

    Update temporary responsive

--- src/pages/Dashboard/LivechatBar/LivechatBar.tsx
+++ src/pages/Dashboard/LivechatBar/LivechatBar.tsx
@@ -1,6 +1,6 @@
 const LivechatBar = () => {
     return (
-        <aside className="w-1/5 bg-[var(--color-form-gray)] min-h-screen">
+        <aside className="hidden lg:block lg:w-1/5 bg-[var(--color-form-gray)] min-h-screen">
 
         </aside>
     )
--- src/pages/Dashboard/MenuBar/MenuBar.tsx
+++ src/pages/Dashboard/MenuBar/MenuBar.tsx
@@ -1,6 +1,6 @@
 const MenuBar = () => {
     return (
-        <aside className="w-1/5 bg-[var(--color-form-gray)] min-h-screen">
+        <aside className="w-1/8 lg:block lg:w-1/5 bg-[var(--color-form-gray)] min-h-screen">
 
         </aside>
     )
