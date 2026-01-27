9e61692b0eb541cf8309775c86e80323adc9bd80
Siuol <nguyencaotan03072000@gmail.com>
2026-01-27 22:20:48 +0200

    Modify light and camera point brightness

--- src/pages/Game/components/GameTable.tsx
+++ src/pages/Game/components/GameTable.tsx
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import tableCover from "../../../assets/TableCover.png";
+import { Deck } from "./Deck";
 
 export const GameTable = () => {
   return (
@@ -14,9 +15,10 @@ export const GameTable = () => {
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat"
     }}>
-      <Canvas camera={{ position: [0, -8, 10], fov: 75 }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[0, 10, 10]} intensity={1} />
+      <Canvas camera={{ position: [0, -3, 10], fov: 75 }}>
+        <ambientLight intensity={1.0} />
+        <pointLight position={[0, 0, 10]} intensity={2} />
+        <Deck/>
       </Canvas>
     </div>
   );
