b61936df5dea62dbca4a1282c4aa2dcb6066cbca
Siuol <nguyencaotan03072000@gmail.com>
2026-01-27 22:13:41 +0200

    Update config and deck component + utils createArc for card flight path

--- src/pages/Game/components/Deck.tsx
+++ src/pages/Game/components/Deck.tsx
@@ -1,12 +1,15 @@
-import {useTexture} from '@react-three/drei';
-import cardBackImage from '../../../assets/game/game-card-back.png';
-import { CARD_SIZE, DECK_POSITION } from '../constants/gameConfig';
+import { useTexture } from "@react-three/drei";
+import cardBackImage from "../../../assets/game/game-card-back.png";
+import { CARD_SIZE, DECK_POSITION } from "../constants/gameConfig";
 
 export const Deck = () => {
-    const backCard = useTexture(cardBackImage);
-    return (
-        <mesh position={[DECK_POSITION.x, DECK_POSITION.y, DECK_POSITION.z]}>
-            <boxGeometry args={([CARD_SIZE.WIDTH, CARD_SIZE.HEIGHT, CARD_SIZE.DEPTH])}
-        </mesh>
-    )
-}
+  const backCard = useTexture(cardBackImage);
+  return (
+    <mesh position={[DECK_POSITION.x, DECK_POSITION.y, DECK_POSITION.z]}>
+      <boxGeometry
+        args={[CARD_SIZE.WIDTH, CARD_SIZE.HEIGHT, CARD_SIZE.DEPTH]}
+      />
+      <meshStandardMaterial map={backCard} />
+    </mesh>
+  );
+};
