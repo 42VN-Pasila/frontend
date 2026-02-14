b60c4f2f956660d209b9915efa8b184d7cb3600e
42.caonguye <nguyencaotan03072000@gmail.com>
2026-02-14 23:44:33 +0200

    Remove draft to recreate new display

--- src/pages/Game/types/CardType.ts
+++ src/pages/Game/types/CardType.ts
@@ -2,16 +2,8 @@ export interface CardType {
   id: string;
   suit: 'hearts' | 'diamonds' | 'spades' | 'clubs';
   rank: '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
-  position: {
-    x: number;
-    y: number;
-    z: number;
-  };
-  rotation: {
-    x: number;
-    y: number;
-    z: number;
-  };
+  position: Position;
+  rotation: Position;
   owner: 1 | 2 | 3 | 4;
   inDeck: boolean;
   cardIndex: number;
